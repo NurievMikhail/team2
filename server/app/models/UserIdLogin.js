6104f4d9bce2bd2b5393fb0f0e091f634da2439d
NurievMikhail <nurievmm@yandex.ru>
2018-05-03 23:37:59 +0500

    Update UserIdLogin.js

--- server/app/models/UserIdLogin.js
+++ server/app/models/UserIdLogin.js
@@ -1,6 +1,1 @@
-'use strict';
 
-const { makeModel } = require('hruhru');
-const userIdLoginScheme = require('../schemes/userIdLoginScheme');
-
-module.exports = makeModel(userIdLoginScheme, 'allUsers', true);
